842c24d05e393efe714e3ad8b93350df50209e87
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2018-07-03 17:04:41 -0300

    Grant exec and select permissions on all sequences in schema public to web role

--- server/initdb/5-grant-permissions.sh
+++ server/initdb/5-grant-permissions.sh
@@ -14,6 +14,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   GRANT USAGE ON SCHEMA public, auth, jwt, pgcrypto TO ${PGRST_DB_WEB_ROLE};
 
   GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ${PGRST_DB_WEB_ROLE};
+  GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO ${PGRST_DB_WEB_ROLE};
   GRANT SELECT ON ALL TABLES IN SCHEMA auth TO ${PGRST_DB_WEB_ROLE};
 
   -- Allow authenticator to become anonymous or logged user
