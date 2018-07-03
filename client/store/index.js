fcf1a802bc5ad6168e50b114e1541f8c843a0cc0
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2018-07-03 16:08:50 -0300

    Add plugin to persist data inside the store
    Install plugin [vuex-persist][1] to save state in the localStorage.
    
    [1]: https://github.com/championswimmer/vuex-persist

--- client/store/index.js
+++ client/store/index.js
@@ -1,16 +1,30 @@
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+})
 
 export default () => {
   return new Vuex.Store({
+    plugins: [vuexLocal.plugin],
+
     state: {
+      email: '',
       token: '',
     },
+
     getters: {
       isAuthenticated(state) {
         return state.token !== '';
       }
     },
+
     mutations: {
+      setEmail(state, { email }) {
+        state.email = email;
+      },
+
       setToken(state, { token }) {
         state.token = token;
       }
